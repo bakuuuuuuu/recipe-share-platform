15685266e6eaf927500c6574f9e363f717102d74
moonhwhw <heewon22222@naver.com>
2024-04-30 19:18:48 +0900

    c

--- src/page/CorepageStyless.jsx
+++ src/page/CorepageStyless.jsx
@@ -6,12 +6,12 @@ const CorepageStyless = {
       height: '100vh', // 화면 전체 높이를 차지
     },
     centralContentStyle: {
-      width: '800px', // 중앙 영역의 가로 너비 조정
+      width: '600px', // 중앙 영역의 가로 너비 조정
       backgroundColor: 'white', // 중앙 영역의 배경색
       padding: '20px', // 내부 여백
       boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // 그림자 효과 추가
       borderRadius: '8px', // 모서리를 둥글게 만듦
-      
+      marginBottom: '60px',
     },
     sideContentStyle : {
       flex: '1', // 나머지 영역을 균등하게 나눔
