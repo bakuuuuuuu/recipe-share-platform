d4bb6008ef57acbe2cb88a0a01ac61ab12958319
moonhwhw <heewon22222@naver.com>
2024-04-30 12:20:49 +0900

    feat: 글쓰기 기능추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MainPage from './page/MainPage';
 import Posts from './component/Posts';
 import EditProfile from './component/EditProfile';
 import {MyPage} from './page/MyPage';
+import WritingPage from './page/WritingPage';
 
 // git test
 function App() {
@@ -15,12 +16,15 @@ function App() {
       <NavLink to="/mypage">마이페이지</NavLink>
       <NavLink to="/login">로그인 페이지</NavLink>
       <NavLink to="/signup">회원가입 페이지</NavLink>
+      <NavLink to="/Writingpage">글쓰기 페이지</NavLink>
     </nav>
     <Routes>
       <Route path="/" element={<MainPage />} /> {/* 회원가입 페이지 라우트 추가 */}
       <Route path="/signup" element={<SignUpForm />} /> {/* 회원가입 페이지 라우트 추가 */}
       <Route path="/login" element={<LoginForm />} /> {/* 로그인 페이지 라우트 추가 */}
       {/* 필요에 따라 더 많은 라우트를 추가할 수 있습니다 */}
+
+      <Route path='/Writingpage' element={<WritingPage/>} /> 
 
       {/* 마이페이지 추가*/}
       <Route path="/mypage" element={<MyPage />}>
---
+++ src/css/CorepageStyle.jsx
@@ -0,0 +1,45 @@
+const CorepageStyles = {
+    corePageStyle: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', // 화면 전체 높이를 차지
+    },
+    centralContentStyle: {
+      width: '700px', // 중앙 영역의 가로 너비 조정
+      backgroundColor: 'white', // 중앙 영역의 배경색
+      padding: '20px', // 내부 여백
+      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // 그림자 효과 추가
+      borderRadius: '8px', // 모서리를 둥글게 만듦
+    },
+    sideContentStyle : {
+      flex: '1', // 나머지 영역을 균등하게 나눔
+      backgroundColor: 'lightgrey', // 사이드 회색 배경
+      height: '100%', // 높이를 화면 전체로
+    },
+  
+
+    formStyle: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    inputStyle: {
+      marginBottom: '15px',
+      padding: '10px',
+      fontSize: '16px',
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      outline: 'none',
+    },
+    buttonStyle: {
+      padding: '10px 20px',
+      fontSize: '14px',
+      backgroundColor: '#2e8b57',
+      color: 'white',
+      border: 'none',
+      borderRadius: '8px',
+      cursor: 'pointer',
+    },
+  };
+  
+  export default CorepageStyles;
