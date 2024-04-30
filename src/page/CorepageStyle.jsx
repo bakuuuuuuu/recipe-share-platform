e6172e21243cf86a82f197c5723554c922df62b6
moonhwhw <heewon22222@naver.com>
2024-04-30 19:05:37 +0900

    commit

--- src/page/CorepageStyle.jsx
+++ src/page/CorepageStyle.jsx
@@ -1,4 +1,4 @@
-const CorepageStyles = {
+const CorepageStyless = {
     corePageStyle: {
       display: 'flex',
       justifyContent: 'center',
@@ -44,4 +44,4 @@ const CorepageStyles = {
     },
   };
   
-  export default CorepageStyles;
+  export default CorepageStyless;
