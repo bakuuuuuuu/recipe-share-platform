1ab8f4b5a696a70aa217fcaa9a08f7e6ae61af8c
moble.yunoh <seo0jjjjj@mail.com>
2024-05-01 18:54:24 +0900

    feat: 수정

--- src/component/EditComponant.js
+++ src/component/EditComponant.js
@@ -1,6 +1,8 @@
 const style = {
   title : {
+    width: "100px",
     fontWeight: 700,
+    textAlign: "end",
     fontSize: "16px",
   },
   subtitleContainer : {
@@ -15,16 +17,18 @@ const style = {
   },
 
   subtitle : {
-    // fontFamily: `"Noto Sans KR", sans-serif;`, 
+    fontFamily: `"Noto Sans KR", sans-serif;`, 
     marginLeft : "10px",
-    display: "block",
     height : "20px",
-
     backgroundColor : "white",
     border: "none",
     borderBottom: "1px solid black",
     fontSize : "13px",
+    textAlign: "center",
+
+    width: "150px",  /* 너비 조정 */
   }
+  
 }
 
 export function EditComponant(props){
@@ -32,7 +36,7 @@ export function EditComponant(props){
 
   return (<><h3>{title} 설정</h3>
   <div style={style.container}>
-  <p style={style.title}>{title}</p>
+  <p style={style.title}>{title} : </p>
   <div style={style.subtitleContainer}/>
   <input style={style.subtitle} value={value}/> 
   </div></>);
