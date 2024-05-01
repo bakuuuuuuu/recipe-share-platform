dd639006b8df65cc85e435c0d517ac718636f7b9
moble.yunoh <seo0jjjjj@mail.com>
2024-05-01 12:40:27 +0900

    작성중인 게시글 목록 기능구현

--- src/domain/userData.js
+++ src/domain/userData.js
@@ -1,10 +1,12 @@
-export const createUserData(id,name,){
-  this.id = 
-  this.password,
-  this.email,
-  this.name,
-  this.phoneNumber,
-  this.gender,
-  this.category,
-  this.role
+export const createUserData = (elements) => {
+  this.id =  elements[0];
+  this.password = elements[1];
+  this.email = elements[2];
+  this.name = elements[3];
+  this.phoneNumber = elements[4];
+  this.gender = elements[5];
+  this.category = elements[6];
+  this.role = elements[7];
 }
+
+
