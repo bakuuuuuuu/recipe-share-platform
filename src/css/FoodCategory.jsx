20e11a57e8dbcab100a3615620e6b4fcd60d7f2f
kang-minjune <bkokmj0327@gmail.com>
2024-05-02 20:45:14 +0900

    FoodCategory수정

--- src/css/FoodCategory.jsx
+++ src/css/FoodCategory.jsx
@@ -5,7 +5,7 @@ import UnderMenu from '../page/UnderMenu';
 import RecipeList from './RecipeList'; // 레시피 리스트 컴포넌트 불러오기
 
 const categories = [
-  { name: '전체', image: 'image/all.jpg' },
+  { name: '전체', image: 'image/all.jpg'},
   { name: '한식', image: 'image/korean.jpg' },
   { name: '양식', image: 'image/western.jpg' },
   { name: '중식', image: 'image/chinese.jpg' },
@@ -29,25 +29,23 @@ const FoodCategory = () => {
   }, []);
 
   useEffect(() => {
-    // 검색어가 변경될 때마다 필터링된 레시피를 업데이트합니다.
-    const filtered = recipes.filter(recipe =>
-      recipe.content.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      recipe.savedUserId.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    // 검색어나 카테고리가 변경될 때마다 필터링된 레시피를 업데이트합니다.
+    let filtered = recipes;
+    if (searchTerm) {
+      filtered = filtered.filter(recipe =>
+        recipe.content.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        recipe.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        recipe.savedUserId.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    }
+    if (activeCategory && activeCategory !== '전체') {
+      filtered = filtered.filter(recipe => recipe.category === activeCategory);
+    }
     setFilteredRecipes(filtered);
-  }, [searchTerm, recipes]);
+  }, [searchTerm, activeCategory, recipes]);
 
   // 카테고리 이름에 따라 레시피를 필터링하는 함수
   const filterRecipesByCategory = (categoryName) => {
-    let filtered;
-    if (categoryName === '전체') {
-      filtered = recipes;
-    } else {
-      filtered = recipes.filter(recipe => recipe.category === categoryName);
-    }
-    setFilteredRecipes(filtered);
-    // 선택된 카테고리를 설정
     setActiveCategory(categoryName);
   };
 
@@ -77,7 +75,12 @@ const FoodCategory = () => {
 
   return (
     <div>
-      <Search onChange={(event) => setSearchTerm(event.target.value)} />
+     <Search
+        recipes={recipes} // 레시피 데이터를 전달
+        activeCategory={activeCategory} // 현재 선택된 카테고리를 전달
+        onSearchTermChange={(searchTerm) => setSearchTerm(searchTerm)} // 검색어 변경 핸들러 전달
+        onCategoryChange={(categoryName) => filterRecipesByCategory(categoryName)} // 카테고리 변경 핸들러 전달
+      />
       <div style={styles.container}>
         {categories.map(category => (
           <FoodCategoryButton
