694a08efaf950ea53b013ea11d12a4883d719ed0
moble <dudwls5674@naver.com>
2024-05-03 10:13:23 +0900

    adsf

--- src/domain/userData.js
+++ src/domain/userData.js
@@ -18,9 +18,9 @@ export function initUesrData() {
     console.log("initalize UserData");
     userArray.push(new UserData("admin", "1234", "test@gmail.com", "서윤오", "010-1234-5678", "male", "한식", "admin"));
     userArray.push(new UserData("user", "1234", "test@gmail.com", "황영진", "010-1234-5678", "male", "한식", "user"));
-    userArray.push(new UserData("test1", "1234", "test@gmail.com", "테스트1", "010-1234-5678", "male", "한식", "user"));
+    userArray.push(new UserData("test1", "1234", "test@gmail.com", "테스트1", "010-1234-5678", "female", "한식", "user"));
     userArray.push(new UserData("test2", "1234", "test@gmail.com", "테스트2", "010-1234-5678", "male", "한식", "user"));
-    userArray.push(new UserData("test3", "1234", "test@gmail.com", "테스트3", "010-1234-5678", "male", "한식", "user"));
+    userArray.push(new UserData("test3", "1234", "test@gmail.com", "테스트3", "010-1234-5678", "female", "한식", "user"));
     userArray.push(new UserData("test4", "1234", "test@gmail.com", "테스트4", "010-1234-5678", "male", "한식", "user"));
     localStorage.setItem("userData", JSON.stringify(userArray));
   }
